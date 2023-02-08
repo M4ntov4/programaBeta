f8f5bc1b825284bc35beae6870e024a30ff20446
senho <senho@SERVER>
2023-02-08 00:22:10 -0300

    Adicionar arquivos de projeto.

---
+++ programaBeta/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programaBeta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+
+
+            Console.Write("Teste");
+            texto = Console.ReadLine();
+
+
+            Console.Write("é isso ai: "+ texto);
+            Console.ReadKey();
+
+        }
+    }
+}
